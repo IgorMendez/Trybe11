42b9fb6f1c745f75f71c06d4e99d4701e7b5ab2d
IgorMendez <igor.emp@hotmail.com>
2021-04-06 21:53:36 -0300

    Resolução exercício 8

--- trybe-exercises/exercises/5.2/javascript-dom-eventos-e-web-storage_2/src/script.js
+++ trybe-exercises/exercises/5.2/javascript-dom-eventos-e-web-storage_2/src/script.js
@@ -13,18 +13,28 @@ mainContent.appendChild(centerContent);
 centerContent.classList.add('center-content');
 
 const criaP = document.createElement('p');
-centerContent.appendChild(criaP)
-criaP.innerHTML = 'Aprendendo a aprender'
+centerContent.appendChild(criaP);
+criaP.innerHTML = 'Aprendendo a aprender';
 
-const leftContent = document.createElement('div')
-mainContent.appendChild(leftContent)
-leftContent.classList.add('left-content')
+const leftContent = document.createElement('div');
+mainContent.appendChild(leftContent);
+leftContent.classList.add('left-content');
 
-const rightContent = document.createElement('div')
-mainContent.appendChild(rightContent)
-rightContent.classList.add('right-content')
+const rightContent = document.createElement('div');
+mainContent.appendChild(rightContent);
+rightContent.classList.add('right-content');
 
-const smallImage = document.createElement('img')
-leftContent.appendChild(smallImage)
-smallImage.src = 'https://picsum.photos/200'
-smallImage.classList.add('small-image')
+const smallImage = document.createElement('img');
+leftContent.appendChild(smallImage);
+smallImage.src = 'https://picsum.photos/200';
+smallImage.classList.add('small-image');
+
+const list = document.createElement('ul');
+rightContent.appendChild(list);
+
+(function createLi(x) {
+  for (let index = 0; index < 10; index++) {
+    let item = document.createElement('li');
+    list.appendChild(item);
+  }
+}());

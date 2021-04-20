5f383756e9c51eea3573be560fc9c71a33b00440
IgorMendez <igor.emp@hotmail.com>
2021-04-19 21:32:28 -0300

    Organizando e adicionando projetos realizados

--- trybe-exercises/bloco_4/4.2/Ex_2.js
+++
@@ -1,8 +0,0 @@
-// Para o segundo exercício, você deve somar todos os valores contidos no array e imprimir o resultado;
-
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-
-let sum = numbers.reduce(function (sumArr, num) {
-  return sumArr + num;
-})
-console.log(sum);
--- trybe-exercises/bloco_4/4.2/Ex_9.js
+++
@@ -1,8 +0,0 @@
-// Utilizando for , crie uma array que vá de 1 até 25 e imprima o resultado;
-
-let arr = []
-
-for (index = 0; index < 25; index += 1) {
-  arr.push(1 + index)
-  console.log(arr[index] * 2)
-}

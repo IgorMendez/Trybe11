1ccc2ae0232e6b045646ac1e93e921b326c5dfa7
IgorMendez <igor.emp@hotmail.com>
2021-04-26 19:12:36 -0300

    Exercícip 1 part 2

--- trybe-exercises/exercises/bloco_7/script.js
+++ trybe-exercises/exercises/bloco_7/script.js
@@ -1,3 +1,7 @@
+// Parte 1
+
+// Exercício 1
+
 const testingScope = (escopo) => {
     if (escopo === true) {
       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -10,8 +14,18 @@ const testingScope = (escopo) => {
 
 testingScope(true);
 
+// 'Exercício 2'
+
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
 oddsAndEvens.sort((a, b) => a - b)
 
 console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+
+// Parte 2
+
+// Exercicio 1
+
+const fac = (n) => n === 0 ? n = 1 : n * fac (n - 1);
+
+console.log(fac(3))

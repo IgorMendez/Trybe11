cb2ccaf360aa2a56e5d53555c0a3736bb6577d2a
IgorMendez <igor.emp@hotmail.com>
2021-04-26 15:57:49 -0300

    Exercício 2 Parte 1

--- trybe-exercises/exercises/bloco_7/script.js
+++ trybe-exercises/exercises/bloco_7/script.js
@@ -9,3 +9,9 @@ const testingScope = (escopo) => {
   }
 
 testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort((a, b) => a - b)
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);

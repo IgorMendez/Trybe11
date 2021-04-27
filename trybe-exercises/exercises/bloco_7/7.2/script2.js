c6da39864b675f05f1e8b4d46627fc4c79281201
IgorMendez <igor.emp@hotmail.com>
2021-04-27 19:25:02 -0300

    Exercicio 5

--- trybe-exercises/exercises/bloco_7/7.2/script2.js
+++ trybe-exercises/exercises/bloco_7/7.2/script2.js
@@ -20,23 +20,34 @@ const lesson3 = {
   turno: 'noite',
 };
 
+const allLessons = {}
+
 // Exercício 1
 
 const addTurno = (lesson, key, value) => lesson[key] = value
 addTurno(lesson2, 'turno' , 'manhã')
-console.log(lesson2)
+// console.log(lesson2)
 
 // Exercício 2
 
 const listKeys = (obj) => Object.keys(obj)
-console.log(listKeys(lesson3))
+// console.log(listKeys(lesson3))
 
 // Exercicío 3
 
 const objLength = (obj) => Object.keys(obj).length
-console.log(objLength(lesson3))
+// console.log(objLength(lesson3))
 
 // Exercicio 4
 
 const objvalues = (obj) => Object.values(obj)
-console.log(objvalues(lesson3))
+// console.log(objvalues(lesson3))
+
+// Exercício 5
+
+const objAssing = (obj, objAdd) =>  Object.assign(obj, objAdd)
+objAssing(allLessons, {lesson1, lesson2, lesson3})
+console.log(allLessons)
+
+// Exercício 6
+

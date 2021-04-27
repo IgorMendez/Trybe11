7a86b31cd66a8a684ed54954815c975b41461682
IgorMendez <igor.emp@hotmail.com>
2021-04-27 18:08:58 -0300

    Exercício 3 - 7.2

---
+++ trybe-exercises/exercises/bloco_7/7.2/script2.js
@@ -0,0 +1,32 @@
+// Parte 2
+
+const lesson1 = {
+  materia: 'Matemática',
+  numeroEstudantes: 20,
+  professor: 'Maria Clara',
+  turno: 'manhã',
+};
+
+const lesson2 = {
+  materia: 'História',
+  numeroEstudantes: 20,
+  professor: 'Carlos',
+};
+
+const lesson3 = {
+  materia: 'Matemática',
+  numeroEstudantes: 10,
+  professor: 'Maria Clara',
+  turno: 'noite',
+};
+
+// Exercício 1
+
+const addTurno = (lesson, key, value) => lesson[key] = value
+addTurno(lesson2, 'turno' , 'manhã')
+console.log(lesson2)
+
+// Exercício 2
+
+const listKeys = (obj) => Object.keys(obj)
+console.log(listKeys(lesson3))

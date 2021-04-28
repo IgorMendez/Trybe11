c58bbb4bd8726b20779faf92df068e0fb81784f0
IgorMendez <igor.emp@hotmail.com>
2021-04-28 16:28:58 -0300

    Exercício 2

--- trybe-exercises/exercises/bloco_7/7.3/exercicios.js
+++ trybe-exercises/exercises/bloco_7/7.3/exercicios.js
@@ -6,3 +6,10 @@ const soma = (a,b) => a + b;
 const expected = soma(4, 5)
 
 assert.strictEqual(expected, 9, 'erro')
+
+// Exercício 2
+
+
+const expected = soma(0, 0)
+
+assert.strictEqual(expected, 0, 'erro')

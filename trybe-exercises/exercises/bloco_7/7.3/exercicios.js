92cd0c2f53c4ac49274135254bc245cf3f7a0021
IgorMendez <igor.emp@hotmail.com>
2021-04-28 17:13:18 -0300

    Exercício 1 - Parte III

--- trybe-exercises/exercises/bloco_7/7.3/exercicios.js
+++ trybe-exercises/exercises/bloco_7/7.3/exercicios.js
@@ -1,4 +1,5 @@
 // // Exercício 1
+
 const assert = require('assert');
 
 // function sum(a, b) {
@@ -9,41 +10,65 @@ const assert = require('assert');
 //   return a + b;
 // }
 
-// // const expected = soma(4, 5);
+// const expected = soma(4, 5);
 
 // assert.strictEqual(expected, 9, 'erro');
 
-// // Exercício 2, 3
 
+// // Exercício 2, 3
 
 // const expected = soma(0, 0);
 
 // assert.strictEqual(expected, 0, 'erro');
 
 
-// Parte 2 
-// Exercício 1
+// // Parte 2 
+// // Exercício 1
 
-function myRemove(arr, item) {
-  let newArr = [];
-  for (let index = 0; index < arr.length; index += 1) {
-    if (item !== arr[index]) {
-      newArr.push(arr[index]);
-    }
-  }
-  return newArr;
-}
+// function myRemove(arr, item) {
+//   let newArr = [];
+//   for (let index = 0; index < arr.length; index += 1) {
+//     if (item !== arr[index]) {
+//       newArr.push(arr[index]);
+//     }
+//   }
+//   return newArr;
+// }
 
-let expected = myRemove([1, 2, 3, 4], 3);
+// let expected = myRemove([1, 2, 3, 4], 3);
 
 // assert.deepStrictEqual(expected ,[1 ,2 ,4] , 'erro')
 
-// Exercício 2
+
+// // Exercício 2
 
 // assert.notDeepStrictEqual(expected ,[1, 2, 3, 4] , 'erro')
 
-// Exercício 3
 
-assert.ok(expected ,[1, 2, 3, 4] );
+// // Exercício 3
+
+// assert.ok(expected ,[1, 2, 3, 4] );
+
+
+// // Parte III
+// // Exercício 1
+
+
+
+function myRemoveWithoutCopy(arr, item) {
+  for (let index = 0, len = arr.length; index < len; index += 1) {
+    if (arr[index] === item) {
+      arr.splice(index, 1);
+      index -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+
+let expected = myRemoveWithoutCopy([1, 2, 3, 4], 3)
+
+assert.deepStrictEqual(expected, [1, 2, 4])
 
 
